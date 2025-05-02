203d2810de91c12fe06c0cfe4994dc9a48f9a503
Daniel07-10 <daniel.banol58@gmail.com>
2025-05-02 13:20:25 -0500

    Inicializando repo

---
+++ src/Layouts/Main/Main.jsx
@@ -0,0 +1,11 @@
+export const Main = () => {
+  return (
+    <>
+      <div className="w-full flex pl-20 flex-col justify-center items-start mt-30 gap-8">
+        <h2 className="text-black text-5xl font-black">Proyecto ZenTravel</h2>
+        <p className="text-black w-[40rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, autem. Fuga sequi saepe minus pariatur earum commodi quaerat praesentium ea dignissimos facere nulla reprehenderit hic assumenda aut facilis, aspernatur dicta.</p>
+        <button className="bg-[#F0FA39] hover:bg-[#D4E02E] cursor-pointer  rounded-4xl w-40 h-10 font-bold ml-15">Descúbrelo</button>
+     </div>
+    </>
+  )
+}
